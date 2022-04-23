a29e19ef38808ebede046b28774b6dc4a28b1eab
amod1811 <ammch101@gmail.com>
2022-04-23 22:14:59 +0530

    Add new contact in Address Book

---
+++ AddressBookMain.java
@@ -0,0 +1,16 @@
+package com.bridgelabzaddressbook;
+/*
+ * added new contact details in contact book
+ * @mod
+ */
+import java.util.Scanner;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		System.out.println(".............Welcome to AddressBook Management System...................");
+		Address address = new Address();
+		address.addContact();
+		address.display();
+	}
+}
