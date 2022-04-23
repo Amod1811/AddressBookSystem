8414c3b600ba9e20b1b82e7fd4c1620ab4ca1098
amod1811 <ammch101@gmail.com>
2022-04-24 00:31:11 +0530

    Added : Ability to Add multiple AddressBook

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -10,6 +10,7 @@ public class AddressBookMain {
 	public static void main(String[] args) {
 		System.out.println(".............Welcome to AddressBook Management System...................");
 		Address address = new Address();
+		// method use to get option
 		address.getmenu();
 		address.addContact();
 		address.display();
